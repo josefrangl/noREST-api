dfdf29b53662cdea4c1cab1bd0832a4d7e546d64
alandouglas96 <alandouglas96@gmail.com>
2020-01-25 12:52:04 +0100

    Added process.env configuration on mongodb.js

--- db/mongodb/mongodb.js
+++ db/mongodb/mongodb.js
@@ -5,13 +5,22 @@ Using Homebrew:
 ## Install mongodb-community (latest version) --->   brew install mongodb-community@4.2
 ## Start the service in brew ---> brew services start mongodb-community
 
-## Optional: Run mongo to check dbs ---> mongo ---> show dbs
+
+## Run mongo on terminal to check dbs ---> mongo
+## In mongo: To check your dbs ---> >show dbs
+## Create db locally if not existing already ---> >use norest
+
+## Ready to go!
+
 */
 
-const mongoose = require('mongoose'); //collections are created dynamically??
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const url = 'mongodb://localhost:27017/norest';
+const base_url = process.env.MONGODB_URL || 'mongodb://localhost:27017/';
+const dbName = process.env.MONGODB_NAME || 'norest';
+
+const url = `${base_url}${dbName}`;
 
 mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true});
 mongoose.connection.once('open', () => console.log(`Connected to mongo at ${url}`));
