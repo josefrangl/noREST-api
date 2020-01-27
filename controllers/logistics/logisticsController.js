4818ec5b095e89529fda31780f795ac4c8ea61bc
elliefairholm <elliefairholm@gmail.com>
2020-01-27 12:45:45 +0100

    ready to rebase

--- controllers/logistics/logisticsController.js
+++ controllers/logistics/logisticsController.js
@@ -4,14 +4,15 @@ const crypto = require('crypto');
 const createModel = require('../../utils/modelGenerators').createModel;
 
 const ApiModel = require('../../models/logistics/logisticsModel');
+const redis = require('../../db/redis/redis');
 
-
+const redisPrefix = 'api-';
 
 
 exports.createApi = async ctx => {
-  
+
   const data = ctx.request.body;
-  const result = await createModel(data); 
+  const result = await createModel(data);
 
   // generate access keys (To be model)
   const apiKey = uuidv1();
@@ -20,20 +21,27 @@ exports.createApi = async ctx => {
   console.log(apiKey, apiSecretKey);
 
   try {
-    const api = await ApiModel.create({
-      api_name: data.api.name,
-      description: data.api.description,
-      user: data.user.id,
-      api_key: apiKey,
-      api_secret_key: apiSecretKey
-    });
+    const exists = await redis.get(redisPrefix + data.api.name);
+    if (exists) {
+      ctx.body = 'An api with this name already exists';
+      ctx.status = 202;
+    } else {
+      const redisApi = redis.set(redisPrefix + data.api.name, `${apiKey}:${apiSecretKey}`);
+      const api = await ApiModel.create({
+        api_name: data.api.name,
+        description: data.api.description,
+        user: data.user.id,
+        api_key: apiKey,
+        api_secret_key: apiSecretKey
+      });
 
-    ctx.body = api;
-    ctx.status = 201;
+      ctx.body = api;
+      ctx.status = 201;
+    }
 
   } catch (error) {
     console.log('Error saving api to the database: ', error);
-    ctx. status = 400;
+    ctx.status = 400;
   }
 };
 
@@ -71,7 +79,7 @@ exports.getUserApis = async ctx => {
   userId = ctx.params.user_id;
   try {
     const userApis = await ApiModel.find({ user: userId });
-      ctx.status = 200;
+    ctx.status = 200;
     if (userApis) {
       ctx.body = userApis;
     } else {
