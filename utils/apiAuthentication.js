eda90244a89de2157c075e7f8ffd6364ba47d2d0
Jose Fran <josefrangl@me.com>
2020-01-28 20:30:44 +0100

    fix: fixed bug in the api middleware auth

--- utils/apiAuthentication.js
+++ utils/apiAuthentication.js
@@ -14,7 +14,7 @@ const authenticateAccess = async (ctx, next) => {
 
     if (public === 'true' && ctx.request.method === 'GET') {
       await next();
-    } else if (public === 'false' && ctx.request.headers.api_key === apiKey && ctx.request.headers.api_secret_key === apiSecretKey) {
+    } else if (ctx.request.headers.api_key === apiKey && ctx.request.headers.api_secret_key === apiSecretKey) {
       await next();
     } else {
       ctx.body = 'You do not have the right permissions to access this api.'
